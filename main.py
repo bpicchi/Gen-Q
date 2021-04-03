4702697794f3dc23da21c6ec4f3dc66d1809aef0
Blake Wheaton <bwheato@emory.edu>
2021-04-03 12:40:06 -0400

    cleaned main,

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+# nltk imports
 from nltk.wsd import lesk
 from nltk.corpus import wordnet as wn
 from nltk.corpus import stopwords
@@ -6,126 +7,92 @@ from nltk.tokenize import word_tokenize
 from nltk import pos_tag
 from nltk import RegexpParser
 
+# file management/data imports
 import os
 import csv
 import pandas as pd
 import numpy as np
 import seaborn as sns
+
+# sklearn imports
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from skmultilearn.problem_transform import BinaryRelevance
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
-
-from sentence_transformers import SentenceTransformer
 from skmultilearn.problem_transform import BinaryRelevance
-
 from sklearn.naive_bayes import GaussianNB
 
-from csv import DictReader
+# sentence_transformer import
+from sentence_transformers import SentenceTransformer
 
-stop_words = set(stopwords.words('english'))
 
-
-def read_csv_col(column: str) -> list:
-	c = []
-
-	with(open("data_file_br_50k.csv")) as file:
-		c = [row[column] for row in DictReader(file)]
-
-	return c
-
-# lemmatize each word and take out stop words
+# reads csv data file and returns list of all quotes
+def get_quotes() -> list:
+	df = pd.read_csv('data_file_br_50k.csv', encoding='cp1252')
+	return df['quote']
 
 
 def lemmatize(words: list) -> list:
 	lemmatized = []
-	punctuation = ['.', ',', '?', '!', ';', ':', '-', "n't"]
+	punctuation = ['.', ',', '?', '!', ';', ':', '-', "n't", '_', '*', ')', '(', '$', '~', '`', '@', '#', '%', '^', '&', ']', '[', '{', '}']
 	stops = stopwords.words('english')
-
 	lemmatizer = WordNetLemmatizer()
 
 	for word in words:
 		if word not in stops and word not in punctuation:
 			lemmatized.append(lemmatizer.lemmatize(word.lower()))
-
+	
 	return lemmatized
-
-
-def extract_keys(words: list) -> list:
-	no_stops = []
-	special_chars = [',', '"', "'", '.', '!',
-		'?', ';', ':', '-', '_', '*', '$', '~', '`']
-
-	for w in words:
-		if w not in stop_words and w not in special_chars:
-			no_stops.append(w)
-
-	return no_stops
 
 
 def cleanup(sentence: str) -> list:
 	# tokenize sentence
 	words = word_tokenize(sentence)
 
-	# lemmatize words
-	lemm_words = lemmatize(words)
-
-	# remove stop words
-	cleaned_words = extract_keys(lemm_words)
+	# lemmatize
+	cleaned_words = lemmatize(words)
 
 	return cleaned_words
 
 
-def simple_tag(words: list) -> list:
-	tagged = pos_tag(words)
+def get_data(column: str) -> list:
+	df = pd.read_csv('data_file_br_50k.csv', encoding='cp1252')
 
-	# tagged structure: (word, tag)
-	# ex. ('I', 'PRP') and ('like', 'VBP')
+	return df[column]
 
-	simple_tags = []
+def cosine(u, v):
+	return np.dot(u,v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
-	for tag in tagged:
-		word = tag[0]
-		pos = tag[1]
+def binary_relevance(quotes: list):
+	print('Loading model...')
 
-		if pos[0] == 'V' or pos[0] == 'N' and not pos == 'NNP':
-			simple_tags.append((word, pos[0].lower()))
-		else:
-			simple_tags.append((word, 'NA'))
+	model = SentenceTransformer('stsb-distilbert-base')
 
-	return simple_tags
+	print('Model loaded.')
+	print('Encoding database of quotes...')
 
+	sentence_embeddings = model.encode(quotes)
+	
+	print('Encoded quotes.')
 
-def get_data() -> list:
-    quotes = read_csv_col('quote')
+	query = "My family left me and I am sad"
+	query_vec = model.encode([query])[0]
 
-	return quotes
-
-
-def merge_tokens(words: list) -> str:
-	merged = ""
-	for index, word in enumerate(words):
-		if index == len(words)-1:
-			merged += word.lower()
-		else:
-			merged += word.lower() + " "
-
-	return merged
+	# print similarity between input (query) and each quote in data
+	# for q in quotes:
+	# 	sim = cosine(query_vec, model.encode([q])[0])
+	# 	print("Quote = ", q, "\tSimilarity = ", sim)
 
 
 def main():
-    sentence = 'I am going to the river bank where I will swim and look at fish'
-    tokens = cleanup(sentence)
+	print('Retrieving quotes...')
 
+	quotes = get_data('quote')
+	
+	print('Retrieved quotes.')
 
-quotes = get_data()
-	for i in range(1000):
-        print(quotes[i])
-
-    print(tokens)
-
-    sbert_model = SentenceTransformer('stsb-roberta-large')
+	binary_relevance(quotes)
 
 
 if __name__ == "__main__":
