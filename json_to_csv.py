5e1d040b974fd39b810cfc628cc775b12c12f362
Blake Wheaton <bwheato@emory.edu>
2021-04-03 13:25:29 -0400

    added csv converter

---
+++ json_to_csv.py
@@ -0,0 +1,81 @@
+import json
+import csv
+
+
+def json_to_csv_br():
+	with open('wise_all.json') as json_file:
+		data = json.load(json_file)
+
+	data_file = open('data_file_br_50k.csv', 'w')
+	csv_writer = csv.writer(data_file)
+	categories = data.keys()
+
+	category_map = {}
+	index = 0
+	for cat in categories:
+		category_map.update({cat: index})
+		index += 1
+
+	headers = []
+	headers.extend(('category', 'cite', 'quote'))
+	headers.extend(categories)
+
+	csv_writer.writerow(headers)
+
+	num_rows = 0
+
+	for category in categories:
+		for quote_block in data[category]:
+			cite = quote_block['cite']
+			quote = quote_block['quote']
+
+			if num_rows == 50000: return
+
+			try:
+				row = [category, cite, quote]
+				category_index = category_map.get(category)
+				for idx, cat in enumerate(categories):
+					if idx == category_index:
+						row.append(1)
+					else:
+						row.append(0)
+				csv_writer.writerow(row)
+				num_rows += 1
+
+			except Exception as e:
+				csv_writer.writerow([category, 'error', 'error'])
+
+def json_to_csv():
+	with open('wise_all.json') as json_file:
+		data = json.load(json_file)
+
+	data_file = open('data_file2.csv', 'w')
+	csv_writer = csv.writer(data_file)
+	topic_words = data.keys()
+
+	csv_writer.writerow(['topic', 'cite', 'quote'])
+
+	for topic in topic_words:
+		for quote_block in data[topic]:
+			cite = quote_block["cite"]
+			quote = quote_block["quote"]
+			try:
+				csv_writer.writerow([topic, cite, quote])
+			except Exception as e:
+				csv_writer.writerow([topic, 'error', 'error'])
+
+	data_file.close()
+
+
+
+def main():
+	# json_to_csv()
+	json_to_csv_br()
+
+
+
+
+
+
+if __name__ == "__main__":
+	main()
